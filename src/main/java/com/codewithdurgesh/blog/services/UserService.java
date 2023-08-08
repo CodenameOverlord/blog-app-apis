77444bfca35b5f6be3eb9d34300b5b89825c1b19
utsavRipplehire <utsav.datta@ripplehire.com>
2023-08-08 19:05:26 +0530

    beutifying UserService.java

--- src/main/java/com/codewithdurgesh/blog/services/UserService.java
+++ src/main/java/com/codewithdurgesh/blog/services/UserService.java
@@ -5,11 +5,9 @@ import com.codewithdurgesh.blog.payloads.UserDto;
 import java.util.List;
 
 public interface UserService {
-    //
     UserDto createUser(UserDto userDto);
     UserDto updateUser(UserDto userDto, Integer userId);
     UserDto getUserById(Integer userID);
     List<UserDto> getAllUsers();
     void deleteUser(Integer userId);
-
 }
